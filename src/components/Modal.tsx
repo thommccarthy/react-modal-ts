e326561b0e6c224748e4a24f92eb2f162d2671e7
thommccarthy <mccarttj@gmail.com>
2023-06-27 18:06:35 -0400

    remove React import statement from Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useFocusTrap from '../hooks/useFocusTrap';
 import './Modal.css';
 
